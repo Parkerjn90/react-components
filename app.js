1a75a8f79bb479daa08e81bbc307c65200108fb9
Joy <parkerjn90@gmail.com>
2022-04-25 12:18:20 -0400

    refactor grocery list to dynamically rendere an array of groceryItems using new GroceryListItem component

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // TODO
+var GroceryListItem = (props) => (
+  <ul>
+    <li>{props.items[0]}</li>
+    <li>{props.items[1]}</li>
+  </ul>
+);
+
+
+var GroceryList = () => (
+  <div>
+    <h2>Grocery List</h2>
+    <GroceryListItem items={['apples', 'bananas']} />
+  </div>
+
+);
+
+// GroceryList(['apples', 'bananas']);
+
+// ReactDOM.render(<Apples />, document.)
+ReactDOM.render(<GroceryList />, document.getElementById("app"))
